bbc1a533b4a3dd39747d264c90b4469b911097ea
Hai Huynh <Hai.Huynh@mgm-tp.com>
2017-04-28 13:02:39 +0700

    Reformat code

--- OCRTest/src/main/java/edu/sfsu/cs/orange/ocr/DecodeThread.java
+++ OCRTest/src/main/java/edu/sfsu/cs/orange/ocr/DecodeThread.java
@@ -17,41 +17,41 @@
 
 package edu.sfsu.cs.orange.ocr;
 
-import edu.sfsu.cs.orange.ocr.CaptureActivity;
 import android.os.Handler;
 import android.os.Looper;
+
 import java.util.concurrent.CountDownLatch;
 
 /**
  * This thread does all the heavy lifting of decoding the images.
- *
+ * <p>
  * The code for this class was adapted from the ZXing project: http://code.google.com/p/zxing
  */
 final class DecodeThread extends Thread {
 
-  private final CaptureActivity activity;
-  private Handler handler;
-  private final CountDownLatch handlerInitLatch;
+    private final CaptureActivity activity;
+    private final CountDownLatch handlerInitLatch;
+    private Handler handler;
 
-  DecodeThread(CaptureActivity activity) {
-    this.activity = activity;
-    handlerInitLatch = new CountDownLatch(1);
-  }
-
-  Handler getHandler() {
-    try {
-      handlerInitLatch.await();
-    } catch (InterruptedException ie) {
-      // continue?
+    DecodeThread(CaptureActivity activity) {
+        this.activity = activity;
+        handlerInitLatch = new CountDownLatch(1);
     }
-    return handler;
-  }
 
-  @Override
-  public void run() {
-    Looper.prepare();
-    handler = new DecodeHandler(activity);
-    handlerInitLatch.countDown();
-    Looper.loop();
-  }
+    Handler getHandler() {
+        try {
+            handlerInitLatch.await();
+        } catch (InterruptedException ie) {
+            // continue?
+        }
+        return handler;
+    }
+
+    @Override
+    public void run() {
+        Looper.prepare();
+        handler = new DecodeHandler(activity);
+        handlerInitLatch.countDown();
+        Looper.loop();
+    }
 }
